76c0026c811ea91b55f112e6770034d5876b7ffd
Joakim <joakimedvardsen2000@gmail.com>
2022-02-06 15:45:21 +0100

    Finished fetch get request on book page load

--- multi-page-react/multi-page-app/src/pages/book.jsx
+++ multi-page-react/multi-page-app/src/pages/book.jsx
@@ -1,13 +1,29 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom'
 
 function Book() {
+    // Grab book id from url
+    let { id } = useParams()
 
-    let { id } = useParams();
+    const [book, setBook] = useState([])
+
+    const fetchData = () => {
+        fetch('http://localhost:8080/books/' + id)
+            .then(response => {
+                return response.json()
+            })
+            .then(data => {
+                setBook(data)
+            })
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, [])
 
     return (
         <div>
-            <h2>Book with id: {id}</h2>
+            <h3>{book.title}</h3>
         </div>
     )
 }
