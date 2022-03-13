0a830a51381a3e9feb65ccbe7e868a2a1108695a
jKm00 <joakied@stud.ntnu.no>
2022-03-13 18:40:38 +0100

    Almost finished with calculator
    Some bugs when calculating subtraction and division multiple times without entering new numbers

--- personal-testing/react-calculator/src/components/Calculator.jsx
+++ personal-testing/react-calculator/src/components/Calculator.jsx
@@ -1,68 +1,140 @@
-import React, { useState, useEffect } from 'react';
-import Button from '../components/Button';
+import React, { useState, useEffect } from "react";
+import Button from "../components/Button";
 
 const Calculator = () => {
+  const ADDITION = 1;
+  const SUBTRACTION = 2;
+  const MULTIPLICATION = 3;
+  const DIVISON = 4;
 
-  const [input, setInput] = useState('')
-  const [clearable, setClerable] = useState(false)
-  const [log, setLog] = useState([])
-
-  const toggleClerable = () => {
-      setClerable(!clearable)
-  }
+  const [input, setInput] = useState(0);
+  const [log, setLog] = useState([]);
+  const [tmp, setTmp] = useState([]);
+  const [operator, setOperator] = useState();
+  const [clearable, setClearable] = useState(true);
 
   const addValue = (symbol) => {
-      if (!clearable) {
-        setInput(oldValue => oldValue + symbol)
-      } else {
-          setInput(symbol)
-          toggleClerable()
-      }
-  }
+    if (clearable) {
+      clearInput();
+      setClearable(false);
+    }
+    setInput((oldValue) => oldValue + symbol);
+  };
 
   const clearInput = () => {
-      setInput('');
-  }
+    setInput("");
+  };
+
+  const resetInput = () => {
+    setInput(0);
+    setClearable(true);
+  };
 
   const clearAll = () => {
-    setInput('')
-    setLog([])
-  }
+    resetInput();
+    setLog([]);
+    setTmp([]);
+  };
 
-  const addition = () => {
-    setLog(log => [...log, input])
-    console.log(log)
-  }
+  const add = () => {
+    if (input !== "") {
+      setTmp((tmp) => [...tmp, input]);
+      setOperator(ADDITION);
+      setClearable(true);
+    }
+  };
+
+  const subtract = () => {
+    if (input !== "") {
+      setTmp((tmp) => [...tmp, input]);
+      setOperator(SUBTRACTION);
+      setClearable(true);
+    }
+  };
+
+  const multiply = () => {
+    if (input !== "") {
+      setTmp((tmp) => [...tmp, input]);
+      setOperator(MULTIPLICATION);
+      setClearable(true);
+    }
+  };
+
+  const divid = () => {
+    if (input !== "") {
+      setTmp((tmp) => [...tmp, input]);
+      setOperator(DIVISON);
+      setClearable(true);
+    }
+  };
+
+  const print = () => {
+    console.log("TEMP: " + tmp);
+    console.log("LOG: " + log);
+  };
 
   const calculate = () => {
-    console.log("NOT IMPLEMETED: Should take last entry in log together with the value in the input field and make the operation. then display the result")
-  }
+    // Calculate result
+    console.log(input);
+    let result;
+    if (tmp.length > 0) {
+      if (input === 0) {
+        result = parseInt(tmp[tmp.length - 1]);
+      } else {
+        if (operator === ADDITION) {
+          result = parseInt(tmp[tmp.length - 1]) + parseInt(input);
+        } else if (operator === SUBTRACTION) {
+          result = parseInt(tmp[tmp.length - 1]) - parseInt(input);
+        } else if (operator === MULTIPLICATION) {
+          result = parseInt(tmp[tmp.length - 1]) * parseInt(input);
+        } else if (operator === DIVISON) {
+          result = parseInt(tmp[tmp.length - 1]) / parseInt(input);
+        }
+      }
+      // Display result in input field
+      setInput(result);
+      // Add result to log
+      setLog((log) => [...log, result]);
+      setClearable(true);
+    }
+  };
 
-  return(
-    <>
-        <div className="calculator">
-            <input type="text" value={input} readOnly/>
-            <Button label="0" value="0" onClick={addValue}/>
-            <Button label="1" value="1" onClick={addValue}/>
-            <Button label="2" value="2" onClick={addValue}/>
-            <Button label="3" value="3" onClick={addValue}/>
-            <Button label="4" value="4" onClick={addValue}/>
-            <Button label="5" value="5" onClick={addValue}/>
-            <Button label="6" value="6" onClick={addValue}/>
-            <Button label="7" value="7" onClick={addValue}/>
-            <Button label="8" value="8" onClick={addValue}/>
-            <Button label="9" value="9" onClick={addValue}/>
-            <Button label="." value="." onClick={addValue}/>
-            <Button label="+" value="+" onClick={addition}/>
-            <Button label="-" value="-" onClick={addValue}/>
-            <Button label="*" value="*" onClick={addValue}/>
-            <Button label="/" value="/" onClick={addValue}/>
-            <Button label="=" onClick={calculate}/>
-            <Button label="c" onClick={clearInput}/>
-            <Button label="ce" onClick={clearAll}/>
-        </div>
-    </>
-  )
-}
+  return (
+    <div className="wrapper">
+      <div className="calculator">
+        <input type="text" value={input} readOnly />
+        <Button label="0" value="0" onClick={addValue} />
+        <Button label="1" value="1" onClick={addValue} />
+        <Button label="2" value="2" onClick={addValue} />
+        <Button label="3" value="3" onClick={addValue} />
+        <Button label="4" value="4" onClick={addValue} />
+        <Button label="5" value="5" onClick={addValue} />
+        <Button label="6" value="6" onClick={addValue} />
+        <Button label="7" value="7" onClick={addValue} />
+        <Button label="8" value="8" onClick={addValue} />
+        <Button label="9" value="9" onClick={addValue} />
+        <Button label="." value="." onClick={print} />
+        <Button label="+" value="+" onClick={add} />
+        <Button label="-" value="-" onClick={subtract} />
+        <Button label="*" value="*" onClick={multiply} />
+        <Button label="/" value="/" onClick={divid} />
+        <Button label="=" onClick={calculate} />
+        <Button label="c" onClick={resetInput} />
+        <Button label="ce" onClick={clearAll} />
+      </div>
+      <div className="log">
+        <h2>History</h2>
+        <ul>
+          {log
+            .slice(0)
+            .reverse()
+            .map((element, index) => {
+              return <li key={index}>{element}</li>;
+            })}
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default Calculator;
