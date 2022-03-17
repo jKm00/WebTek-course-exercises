a2985d2ce4dc2d73d49760087edc545e54aa84ff
Joakim <joakied@stud.ntnu.no>
2022-03-17 12:52:57 +0100

    Completed exercise 11

--- exercieses/ex10.11/script.js
+++ exercieses/ex10.11/script.js
@@ -1,6 +1,9 @@
+const body = document.querySelector("body");
 const addBtn = document.querySelector("[data-add]");
 const removeBtn = document.querySelector("[data-remove]");
 const tableBody = document.querySelector("table").querySelector("tbody");
+
+const square = document.querySelector("[data-square]");
 
 addBtn.addEventListener("click", (e) => {
   const tr = document.createElement("tr");
@@ -19,3 +22,18 @@ removeBtn.addEventListener("click", (e) => {
     tableBody.removeChild(lastRow);
   }
 });
+
+square.addEventListener("mouseenter", (e) => {
+  const gridSquares = document.querySelectorAll("body, h1, p, td, button");
+  const red = generateRandom(255);
+  const blue = generateRandom(255);
+  const green = generateRandom(255);
+  square.style.backgroundColor = "rgba(" + red + "," + blue + "," + green + ")";
+  gridSquares[generateRandom(gridSquares.length)].appendChild(square);
+});
+
+function generateRandom(maxLimit = 2) {
+  let rand = Math.random() * maxLimit;
+  rand = Math.floor(rand);
+  return rand;
+}
