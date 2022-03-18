8173bc2e73a4dd0f1635e18198b160a1110d2483
jKm00 <joakied@stud.ntnu.no>
2022-03-18 14:19:27 +0100

    Small modification in exercise 10.9

--- exercieses/ex10.9/script.js
+++ exercieses/ex10.9/script.js
@@ -3,5 +3,5 @@ const colorDisplay = document.querySelector(".color-display");
 
 color.addEventListener("change", (e) => {
   colorDisplay.style.backgroundColor = e.target.value;
-  colorDisplay.innerHTML = e.target.value;
+  colorDisplay.innerText = e.target.value;
 });
