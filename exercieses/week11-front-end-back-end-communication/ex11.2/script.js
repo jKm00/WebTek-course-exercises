f68a2f34c73fb3844a96592c681048c48f9ab005
Joakim <joakied@stud.ntnu.no>
2022-03-21 11:19:20 +0100

    Removed unnecessary js script from exercise 11.2

--- exercieses/week11-front-end-back-end-communication/ex11.2/script.js
+++
@@ -1,6 +0,0 @@
-const btn = document.querySelector("button");
-const input = document.querySelector("input");
-
-btn.addEventListener("click", () => {
-  window.location = "https://www.jula.no/search/?query=" + input.value;
-});
