af4630a081b6d764dc218062a105eeaa8347d3e2
jKm00 <joakied@stud.ntnu.no>
2022-05-21 23:24:48 +0200

    Made some progress

--- personal-testing/swiper-testing/src/App.js
+++ personal-testing/swiper-testing/src/App.js
@@ -2,10 +2,18 @@ import React from "react";
 
 import Swiper from "./components/swiper/Swiper";
 
+import "./styles.css";
+
 export default function App() {
   return (
     <div className="app">
-      <Swiper />
+      <Swiper
+        images={[
+          "https://picsum.photos/500/500?random=1",
+          "https://picsum.photos/500/500?random=2",
+          "https://picsum.photos/500/500?random=3",
+        ]}
+      />
     </div>
   );
 }
--- personal-testing/swiper-testing/src/components/swiper/Swiper.jsx
+++ personal-testing/swiper-testing/src/components/swiper/Swiper.jsx
@@ -1,21 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import "./swiper.css";
 
-export default function Swiper() {
+const SWIPER = document.querySelector("[data-swiper]");
+
+export default function Swiper({ images }) {
+  const [swiperHeight, setSwiperHeight] = useState(NaN);
+  const [swiperWidth, setSwiperWidth] = useState(NaN);
+  const [current, setCurrent] = useState(0);
+  const [items, setItems] = useState(
+    document.querySelectorAll("[data-swiper-item]")
+  );
+
+  useEffect(() => {
+    setSwiperHeight(document.querySelector("[data-swiper]").clientHeight);
+    setSwiperWidth(document.querySelector("[data-swiper]").clientWidth);
+  }, []);
+
+  function goPrevious() {}
+
+  function goNext() {
+    setCurrent(current + 1);
+    const translation = -swiperWidth * current;
+    items.forEach((item) => {
+      item.style.transform = "translate(" + translation + "px, 0)";
+    });
+  }
+
   return (
-    <div className="swiper">
-      <button className="swiper__nav-btn swiper__nav-btn--right">&#x3c;</button>
-      <button className="swiper__nav-btn swiper__nav-btn--left">&#x3e;</button>
-      <div className="swiper__item">
-        <img src="https://picsum.photos/200/300" alt="" />
-      </div>
-      <div className="swiper__item">
-        <img src="https://picsum.photos/200/300" alt="" />
-      </div>
-      <div className="swiper__item">
-        <img src="https://picsum.photos/200/300" alt="" />
-      </div>
+    <div className="swiper" data-swiper>
+      <button
+        onClick={goPrevious}
+        className="swiper__nav-btn swiper__nav-btn--left"
+      >
+        &#x3c;
+      </button>
+      <button
+        onClick={goNext}
+        className="swiper__nav-btn swiper__nav-btn--right"
+      >
+        &#x3e;
+      </button>
+      {images.map((img, index) => (
+        <div key={index} className="swiper__item" data-swiper-item>
+          <img src={img} alt="" />
+        </div>
+      ))}
     </div>
   );
 }
